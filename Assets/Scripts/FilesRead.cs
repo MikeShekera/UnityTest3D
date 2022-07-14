82fbfb89dc68b09061225613a8112b13f5632ca2
Mikhail Shekera <shekera.mikhail@gmail.com>
2022-07-14 18:07:18 +0300

    merging lists

--- Assets/Scripts/FilesRead.cs
+++ Assets/Scripts/FilesRead.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 
 public class FilesRead : MonoBehaviour
 {
-    private void Start()
+    public void Start()
     {
         string readFromFilePath = Application.dataPath + "/Resources/" + "data1" + ".txt";
+        string readFromFilePath2 = Application.dataPath + "/Resources/" + "data2" + ".txt";
 
-        List<string> mergedList = File.ReadAllLines(readFromFilePath).ToList(); 
+        List<string> list1 = File.ReadAllLines(readFromFilePath).ToList();
+        List<string> list2 = File.ReadAllLines(readFromFilePath2).ToList();
+        List<string> mergedList = new List<string>();
+        mergedList = list1.Union<string>(list2).ToList<string>();
+
 
         foreach (var x in mergedList)
         {
